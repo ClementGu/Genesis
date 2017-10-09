e38075b6a1fa909ff4176f179a3afd1ed410c39f
ClementGu <gujun0305@126.com>
2017-10-09 19:34:54 +0800

    create Genesis
    base

---
+++ Cenesis/classes/GJ_Pool/GJ_ReadMe.swift
@@ -0,0 +1,16 @@
+
+//
+//  GJ_ReadMe.swift
+//  Cenesis
+//
+//  Created by Clement_Gu on 2017/10/9.
+//  Copyright © 2017年 clement. All rights reserved.
+//
+
+//说明
+/**
+     这个部分主要用来放置公用池
+     1.公用方法
+ 
+ 
+ */
---
+++ Cenesis/classes/GJ_Root/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cenesis
+//
+//  Created by Clement_Gu on 2017/10/9.
+//  Copyright © 2017年 clement. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
